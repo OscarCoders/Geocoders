7aafa6c431989f965bbb42fa756c1b501d583e2e
temo-castro <temocastro9@gmail.com>
2021-12-01 00:12:28 -0600

    login con google 1.1

--- views/LoginScreen.js
+++ views/LoginScreen.js
@@ -1,168 +1,187 @@
-import { useNavigation } from '@react-navigation/core'
-import React, { useEffect, useState } from 'react'
-import { KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
-import { auth } from '../Database/firebase'
+import { useNavigation } from "@react-navigation/core";
+import React, { useEffect, useState } from "react";
+import {
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import { auth } from "../Database/firebase";
 
-import * as Google from 'expo-google-app-auth'
+import * as Google from "expo-google-app-auth";
 
-import firebase from '../Database/firebase'
-
+import firebase from "../Database/firebase";
 
 const LoginScreen = () => {
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [googleSubmitting, setGoogleSubmitting] = useState(false)
+  const [googleSubmitting, setGoogleSubmitting] = useState(false);
 
-    const navigation = useNavigation()
+  const navigation = useNavigation();
 
-    useEffect(() => {
-        const unsubscribe = auth.onAuthStateChanged(user => {
-            if (user) {
-                navigation.replace("MenuScreen")
-            }
-        })
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        navigation.replace("MenuScreen");
+      }
+    });
 
-        return unsubscribe
-    }, [])
+    return unsubscribe;
+  }, []);
 
-    const handleSignUp = () => {
-        auth
-            .createUserWithEmailAndPassword(email, password)
-            .then(userCredentials => {
-                const user = userCredentials.user;
-                console.log('Registered with:', user.email);
-            })
-            .catch(error => alert(error.message))
-    }
+  const handleSignUp = () => {
+    auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Registered with:", user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
 
-    const handleLogin = () => {
-        auth
-            .signInWithEmailAndPassword(email, password)
-            .then(userCredentials => {
-                const user = userCredentials.user;
-                console.log('Logged in with:', user.email);
-            })
-            .catch(error => alert(error.message))
-    }
-    const handleAlert = () => {
-        alert('state: ' + email)
-    }
+  const handleLogin = () => {
+    auth
+      .signInWithEmailAndPassword(email, password)
+      .then((userCredentials) => {
+        const user = userCredentials.user;
+        console.log("Logged in with:", user.email);
+      })
+      .catch((error) => alert(error.message));
+  };
+  const handleAlert = () => {
+    alert("state: " + email);
+  };
 
-    const handleGoogleSingIn = () =>{
-        setGoogleSubmitting(true)
-        const config = {
-            iosClientId: `626556513871-5b1geq4mcb3m9n9a828nlb20ju23hhku.apps.googleusercontent.com`,
-            androidClientId: `626556513871-7gfacgkjt2r9eigi7m308b9cem3c07t2.apps.googleusercontent.com`,
-            scopes: ['profile', 'email']
+  const handleGoogleSingIn = () => {
+    setGoogleSubmitting(true);
+    const config = {
+      iosClientId: `626556513871-5b1geq4mcb3m9n9a828nlb20ju23hhku.apps.googleusercontent.com`,
+      androidClientId: `626556513871-7gfacgkjt2r9eigi7m308b9cem3c07t2.apps.googleusercontent.com`,
+      scopes: ["profile", "email"],
+    };
+
+    Google.logInAsync(config)
+      .then((result) => {
+        const { type, user } = result;
+
+        if (type == "success") {
+          const { email, name, photoUrl } = user;
+          console.log("se compoleto el inicio de sesion");
+          setTimeout(
+            () => navigation.navigate("MenuScreen", { email, name, photoUrl }),
+            1000
+          );
+        } else {
+          console.log("no logueado");
         }
+        setGoogleSubmitting(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setGoogleSubmitting(false);
+      });
+  };
 
-        Google.logInAsync(config).then((result)=>{
-            const{type, user} = result;
+  return (
+    <KeyboardAvoidingView style={styles.container} behavior="padding">
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+          style={styles.input}
+        />
+        <TextInput
+          placeholder="Password"
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+          style={styles.input}
+          secureTextEntry
+        />
+      </View>
 
-            if(type == 'success'){
-                const {email, name, photoUrl} = user
-                console.log("se compoleto el inicio de sesion")
-                setTimeout(() => navigation.navigate('MenuScreen', {email, name, photoUrl}), 1000)
-            }else{
-                console.log("no logueado")
-            }
-            setGoogleSubmitting(false)
-        }).catch(error =>{
-            console.log(error)
-            setGoogleSubmitting(false)
-        })
-    }
-
-    return (
-        <KeyboardAvoidingView
-            style={styles.container}
-            behavior="padding"
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity onPress={handleLogin} style={styles.button}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={handleSignUp}
+          style={[styles.button, styles.buttonOutline]}
         >
-            <View style={styles.inputContainer}>
-                <TextInput
-                    placeholder="Email"
-                    value={email}
-                    onChangeText={text => setEmail(text)}
-                    style={styles.input}
-                />
-                <TextInput
-                    placeholder="Password"
-                    value={password}
-                    onChangeText={text => setPassword(text)}
-                    style={styles.input}
-                    secureTextEntry
-                />
-            </View>
+          <Text style={styles.buttonOutlineText}>Register</Text>
+        </TouchableOpacity>
+        {!googleSubmitting ? (
+          <TouchableOpacity
+            onPress={handleGoogleSingIn}
+            style={[styles.button, styles.buttonOutline]}
+          >
+            <Text style={styles.buttonOutlineText}>
+              iniciar sesion con google
+            </Text>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity
+            onPress={handleGoogleSingIn}
+            style={[styles.button, styles.buttonOutline]}
+            disabled
+          >
+            <Text style={styles.buttonOutlineText}>
+              iniciar sesion con google
+            </Text>
+          </TouchableOpacity>
+        )}
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
 
-            <View style={styles.buttonContainer}>
-                <TouchableOpacity
-                    onPress={handleLogin}
-                    style={styles.button}
-                >
-                    <Text style={styles.buttonText}>Login</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={handleSignUp}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>Register</Text>
-                </TouchableOpacity>
-                <TouchableOpacity
-                    onPress={handleGoogleSingIn}
-                    style={[styles.button, styles.buttonOutline]}
-                >
-                    <Text style={styles.buttonOutlineText}>iniciar sesion con google</Text>
-                </TouchableOpacity>
-            </View>
-        </KeyboardAvoidingView>
-    )
-}
-
-export default LoginScreen
+export default LoginScreen;
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-    inputContainer: {
-        width: '80%'
-    },
-    input: {
-        backgroundColor: 'white',
-        paddingHorizontal: 15,
-        paddingVertical: 10,
-        borderRadius: 10,
-        marginTop: 5,
-    },
-    buttonContainer: {
-        width: '60%',
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 40,
-    },
-    button: {
-        backgroundColor: '#0782F9',
-        width: '100%',
-        padding: 15,
-        borderRadius: 10,
-        alignItems: 'center',
-    },
-    buttonOutline: {
-        backgroundColor: 'white',
-        marginTop: 5,
-        borderColor: '#0782F9',
-        borderWidth: 2,
-    },
-    buttonText: {
-        color: 'white',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-    buttonOutlineText: {
-        color: '#0782F9',
-        fontWeight: '700',
-        fontSize: 16,
-    },
-})
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  inputContainer: {
+    width: "80%",
+  },
+  input: {
+    backgroundColor: "white",
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    borderRadius: 10,
+    marginTop: 5,
+  },
+  buttonContainer: {
+    width: "60%",
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 40,
+  },
+  button: {
+    backgroundColor: "#0782F9",
+    width: "100%",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonOutline: {
+    backgroundColor: "white",
+    marginTop: 5,
+    borderColor: "#0782F9",
+    borderWidth: 2,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+  buttonOutlineText: {
+    color: "#0782F9",
+    fontWeight: "700",
+    fontSize: 16,
+  },
+});
--- views/MenuScreen.js
+++ views/MenuScreen.js
@@ -1,36 +1,73 @@
 // Dependencies
-import { useNavigation } from '@react-navigation/core'
-import React  from "react";
+import { useNavigation } from "@react-navigation/core";
+import React from "react";
+import { ListItem, Avatar } from "react-native-elements";
+import { TouchableHighlight } from "react-native";
 import {
   StyleSheet,
   Text,
   View,
   Button,
   ScrollView,
-  TextInput,BackHandler
+  TextInput,
+  BackHandler,
 } from "react-native";
 import { useState } from "react";
-import  firebase from '../Database/firebase'
-import { auth } from '../Database/firebase'
+import firebase from "../Database/firebase";
+import { auth } from "../Database/firebase";
 
-const MenuScreen=(props)=>{
-  const navigation = useNavigation()
+const MenuScreen = (props) => {
+  const navigation = useNavigation();
   const handleSignOut = () => {
     auth
       .signOut()
       .then(() => {
-        navigation.replace("LoginScreen")
+        navigation.replace("LoginScreen");
       })
-      .catch(error => alert(error.message))
-  }
-    return(
-        <View>
-        <Button title="Consulta de usuarios" onPress= {()=> props.navigation.navigate("userList") } />
-        <Button title="Consulta de cliente(GPS)" onPress= {()=> props.navigation.navigate("GpsLista") } />
-        <Button title="Salir" onPress ={()=>{console.log('clicked'); return BackHandler.exitApp();}}/>
-        <Button title="cerrar sesion" onPress ={handleSignOut}/>
-        </View>
-    );
+      .catch((error) => alert(error.message));
+  };
+  return (
+    <View>
+      <ListItem
+        Component={TouchableHighlight}
+        containerStyle={{}}
+        disabledStyle={{ opacity: 0.5 }}
+        onLongPress={() => console.log("onLongPress()")}
+        onPress={() => console.log("onLongPress()")}
+        pad={20}
+      >
+        <Avatar
+          source={{
+            uri: props.route.params.photoUrl,
+          }}
+        />
+        <ListItem.Content>
+          <ListItem.Title>
+            <Text>Bienvenido {props.route.params.name}</Text>
+          </ListItem.Title>
+          <ListItem.Subtitle>
+            <Text>Email: {props.route.params.email}</Text>
+          </ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+      <Button
+        title="Consulta de usuarios"
+        onPress={() => props.navigation.navigate("userList")}
+      />
+      <Button
+        title="Consulta de cliente(GPS)"
+        onPress={() => props.navigation.navigate("GpsLista")}
+      />
+      <Button
+        title="Salir"
+        onPress={() => {
+          console.log("clicked");
+          return BackHandler.exitApp();
+        }}
+      />
+      <Button title="cerrar sesion" onPress={handleSignOut} />
+    </View>
+  );
 };
 
 export default MenuScreen;
